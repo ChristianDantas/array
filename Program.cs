df846fbb85f7ac698acf0c24d96e4edb60d9add1
Christian <christian_d_ol9000@hotmail.com>
2020-11-20 11:07:05 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Insira o total de compras que você gostaria de fazer: ");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            string[] compras = new string[tamanho];
+
+            // Console.WriteLine(nomes.Length);
+
+            for( int i = 0; i < compras.Length; i++)
+            {
+                Console.Write("Nome do aluno:");
+                compras[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("");
+
+            for( int i = 0; i < compras.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(" - ");
+                Console.Write(compras[i]);
+                Console.Write(" - ");
+            }
+
+        }
+    }
+}
